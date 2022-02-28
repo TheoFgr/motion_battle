c33f0e1903466178f018c7d83bd3587c437d8372
theo faugere <theo.faugere3@gmail.com>
2022-02-28 16:07:14 +0100

    initialisation de la db fin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_145726) do
+ActiveRecord::Schema.define(version: 2022_02_28_150614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,8 +28,22 @@ ActiveRecord::Schema.define(version: 2022_02_28_145726) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "pseudo"
+    t.integer "score"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "waiting_rooms", force: :cascade do |t|
+    t.integer "score"
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_waiting_rooms_on_game_id"
+    t.index ["user_id"], name: "index_waiting_rooms_on_user_id"
+  end
+
+  add_foreign_key "waiting_rooms", "games"
+  add_foreign_key "waiting_rooms", "users"
 end
