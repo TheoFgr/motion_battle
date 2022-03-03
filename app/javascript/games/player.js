174721a37404eeafb51b40ae4e5eaa00fd45a5c8
theo faugere <theo.faugere3@gmail.com>
2022-03-03 16:05:35 +0100

    Player Move

--- app/javascript/games/player.js
+++ app/javascript/games/player.js
@@ -1,4 +1,5 @@
 import player1 from "../images/player_1.png"
+import { MovingDirection } from "./moving_direction";
 
 export default class Player {
   constructor(x, y, tileSize, velocity, tileMap){
@@ -8,12 +9,17 @@ export default class Player {
     this.velocity = velocity;
     this.tileMap = tileMap;
 
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown)
+
     this.#loadPlayerImages();
   }
 
 
   draw(ctx) {
-    // this.#move();
+    this.#move();
     ctx.drawImage(this.playerImages[this.playerImageIndex], this.x, this.y, this.tileSize, this.tileSize)
   }
 
@@ -25,8 +31,67 @@ export default class Player {
     this.playerImageIndex = 0;
   }
 
+  #keydown = (event) => {
+    //up
+    if (event.keyCode == 38) {
+      console.log('up')
+      if (this.currentMovingDirection == MovingDirection.down)
+          this.currentMovingDirection = MovingDirection.up;
+          this.requestedMovingDirection = MovingDirection.up;
+    }
 
+    //down
+    if (event.keyCode == 40) {
+      console.log('down')
+      if (this.currentMovingDirection == MovingDirection.up)
+          this.currentMovingDirection = MovingDirection.down;
+          this.requestedMovingDirection = MovingDirection.down;
+    }
 
+    //left
+    if (event.keyCode == 37) {
+      console.log('left')
+      if (this.currentMovingDirection == MovingDirection.right)
+          this.currentMovingDirection = MovingDirection.left;
+          this.requestedMovingDirection = MovingDirection.left;
+    }
+
+    //right
+    if (event.keyCode == 39) {
+      console.log('right')
+      if (this.currentMovingDirection == MovingDirection.left)
+          this.currentMovingDirection = MovingDirection.right;
+          this.requestedMovingDirection = MovingDirection.right;
+    }
+  }
+
+#move() {
+  if (this.currentMovingDirection !== this.requestedMovingDirection){
+    if(
+      Number.isInteger(this.x / this.tileSize) &&
+      Number.isInteger(this.y / this.tileSize)
+    ){
+      this.currentMovingDirection = this.requestedMovingDirection;
+    }
+  }
+  switch (this.currentMovingDirection) {
+    case MovingDirection.up:
+      this.y -= this.velocity
+      break;
+
+    case MovingDirection.down:
+      this.y += this.velocity
+      break;
+
+    case MovingDirection.left:
+      this.x -= this.velocity
+      break;
+
+    case MovingDirection.right:
+      this.x += this.velocity
+      break;
+  }
+}
 
 
 
