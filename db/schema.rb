5b8b45542092c10bac06f2eeb31b95ec5f98b28a
Thomas Loubet <thommasloubet@hotmail.fr>
2022-03-05 03:07:18 +0100

    setup ranking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_115800) do
+ActiveRecord::Schema.define(version: 2022_03_05_003812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2022_03_01_115800) do
     t.index ["user_id"], name: "index_participations_on_user_id"
   end
 
+  create_table "rankings", force: :cascade do |t|
+    t.integer "total_kills"
+    t.integer "total_wins"
+    t.integer "total_scores"
+    t.time "ingame_times"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,6 +53,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_115800) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "pseudo"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
